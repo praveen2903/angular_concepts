19e2760f4daec8aa30569e48914bc1be7b23d945
praveen2903 <vvit20bq1a05l0@gmail.com>
2026-06-08 18:37:48 +0530

    added interview questions

--- src/app/components/machine-code/machine-code.ts
+++ src/app/components/machine-code/machine-code.ts
@@ -7,8 +7,8 @@ import { Component, ElementRef } from '@angular/core';
   styleUrl: './machine-code.css',
 })
 export class MachineCode {
-   stars = [1,2,3,4,5];
-   clickedIndex = 0
+   stars:number[] = [1,2,3,4,5];
+   clickedIndex = 0;
    rateStars (star:number){
     this.clickedIndex= star;
    }
@@ -54,7 +54,8 @@ getFill(star:number){
   }
   return diff * 100;
 }
-fractionTs = `starRating = 0;
+fractionTs = `stars=[1,2,3,4,5]
+starRating = 0;
 hoverStarRating =0;
 
 selectRating(event: MouseEvent, star: number) {
@@ -162,5 +163,299 @@ clickedTs = `stars = [1,2,3,4,5];
 clickedIndex = 0
 rateStars (star:number){
   this.clickedIndex= star;
+}`;
+
+
+users = ['john', 'peter', 'praveen', 'david', 'sam','sai', 'eren', 'mikasa', 'rokkam'];
+searchText = '';
+
+filteredUsers(){
+  return this.users.filter(user=>user.toLowerCase().includes(this.searchText.toLowerCase()))
+}
+
+searchTs = `users = ['john', 'peter', 'praveen', 'david', 'sam','sai', 'eren', 'mikasa', 'rokkam'];
+searchText = '';
+
+filteredUsers(){
+  return this.users.filter(user=>user.toLowerCase().includes(this.searchText.toLowerCase()))
+}`;
+searchCode = `<input [(ngModel)]="searchText" placeholder="search ..."  />
+<ul>
+  <li *ngFor="let user of filteredUsers()">
+    {{user}}
+  </li>
+</ul>`
+
+todos : string[] =[];
+task: string='';
+editIndex : number|null =null;
+
+addTask(){
+  if(!this.task.trim()) return ;
+  if(this.editIndex !== null){
+    this.todos[this.editIndex]=this.task;
+    this.editIndex=null;
+  } else{
+    this.todos.push(this.task)
+  }
+  this.task = '';
+}
+deleteTask(index: number) {
+  this.todos.splice(index, 1);
+  if(this.editIndex === index){
+    this.editIndex=null;
+    this.task='';
+  }
+}
+updateTask(index:number){
+  this.task = this.todos[index]
+  this.editIndex= index;
+}
+todobasicCode = `todos : string[] =[];
+task: string='';
+editIndex : number|null =null;
+
+addTask(){
+  if(!this.task.trim()) return ;
+  if(this.editIndex !== null){
+    this.todos[this.editIndex]=this.task;
+    this.editIndex=null;
+  } else{
+    this.todos.push(this.task)
+  }
+  this.task = '';
+}
+deleteTask(index: number) {
+  this.todos.splice(index, 1);
+  if(this.editIndex === index){
+    this.editIndex=null;
+    this.task='';
+  }
+}
+updateTask(index:number){
+  this.task = this.todos[index]
+  this.editIndex= index;
+}`;
+todoBasicHtml = `<input type=" 'text" [(ngModel)]="task"/>
+<button (click)="addTask()">
+  {{editIndex !== null ? 'Update': 'Add'}}
+</button>
+<ul>
+  <li *ngFor="let todo of todos; let i = index">
+    {{todo}}
+    <button (click)="updateTask(i)">Edit</button>
+    <button (click)="deleteTask(i)">delete</button>
+  </li>
+</ul>`;
+
+taskItem : string = '';
+todoList:string[] = ['angular', 'react'];
+updateIndex: number |null = null;
+updatingTask = '';
+
+addTodoTask(){
+  if(!this.taskItem.trim()) return;
+  this.todoList.push(this.taskItem);
+  this.taskItem='';
+}
+deleteTodoTask(index:number){
+  this.todoList.splice(index,1);
+}
+startEdit(index:number){
+  this.updateIndex=index;
+  this.updatingTask =this.todoList[index];
+}
+saveEdit(index:number){
+  if(!this.updatingTask.trim()) return;
+  this.todoList[index] = this.updatingTask;
+  this.updateIndex=null;
+  this.updatingTask ='';
+}
+
+cancelEdit(){
+  this.updateIndex= null;
+  this.updatingTask= '';
+}
+
+draggedIndex :number|null = null;
+onDragStart(index:number){
+  this.draggedIndex = index;
+}
+onDrop(targetIndex: number){
+  if(this.draggedIndex === null) return;
+  const draggedItem = this.todoList[this.draggedIndex];
+  this.todoList.splice(this.draggedIndex, 1);
+  this.todoList.splice(targetIndex, 0, draggedItem);
+  this.draggedIndex=null;
+}
+todoHtml = `<div style="display: flex; gap: 20px;">
+  <input type="text" [(ngModel)]="taskItem" (keyup.enter)="addTodoTask()" placeholder="Enter..."/>
+  <button (click)="addTodoTask()">Add </button>
+</div>
+<ul>
+  <li *ngFor="let list of todoList; let i = index">
+    <ng-container *ngIf="updateIndex!==i; else editMode"> 
+      {{list}}
+      <button (click)="startEdit(i)">Edit</button>
+      <button (click)="deleteTodoTask(i)">Delete</button>
+    </ng-container>
+    <!-- //else must in ng-template only; else it gives error-->
+    <ng-template #editMode>
+      <div style="display: flex; gap: 20px;">  
+        <input type="text" [(ngModel)]="updatingTask" placeholder="Edit.." (keyup.enter)="saveEdit(i)"/>
+        <button (click)="saveEdit(i)">save</button>
+        <button (click)="cancelEdit()">Cancel</button>
+      </div>
+    </ng-template>
+  </li>
+</ul>`
+
+todoTs=`taskItem : string = '';
+todoList:string[] = ['angular', 'react'];
+updateIndex: number |null = null;
+updatingTask = '';
+
+addTodoTask(){
+  if(!this.taskItem.trim()) return;
+  this.todoList.push(this.taskItem);
+  this.taskItem='';
+}
+deleteTodoTask(index:number){
+  this.todoList.splice(index,1);
+}
+startEdit(index:number){
+  this.updateIndex=index;
+  this.updatingTask =this.todoList[index];
+}
+saveEdit(index:number){
+  if(!this.updatingTask.trim()) return;
+  this.todoList[index] = this.updatingTask;
+  this.updateIndex=null;
+  this.updatingTask ='';
+}
+
+cancelEdit(){
+  this.updateIndex= null;
+  this.updatingTask= '';
+}
+`
+
+todoDragTs = `taskItem : string = '';
+todoList:string[] = ['angular', 'react'];
+updateIndex: number |null = null;
+updatingTask = '';
+
+addTodoTask(){
+  if(!this.taskItem.trim()) return;
+  this.todoList.push(this.taskItem);
+  this.taskItem='';
+}
+deleteTodoTask(index:number){
+  this.todoList.splice(index,1);
+}
+startEdit(index:number){
+  this.updateIndex=index;
+  this.updatingTask =this.todoList[index];
+}
+saveEdit(index:number){
+  if(!this.updatingTask.trim()) return;
+  this.todoList[index] = this.updatingTask;
+  this.updateIndex=null;
+  this.updatingTask ='';
+}
+
+cancelEdit(){
+  this.updateIndex= null;
+  this.updatingTask= '';
+}
+
+draggedIndex :number|null = null;
+onDragStart(index:number){
+  this.draggedIndex = index;
+}
+onDrop(targetIndex: number){
+  if(this.draggedIndex === null) return;
+  const draggedItem = this.todoList[this.draggedIndex];
+  this.todoList.splice(this.draggedIndex, 1);
+  this.todoList.splice(targetIndex, 0, draggedItem);
+  this.draggedIndex=null;
 }`
+todoDragHtml =`.dragging{
+  background: lightgray;
+  opacity:0.7;
+}
+
+<div style="display: flex; gap: 20px;">
+  <input type="text" [(ngModel)]="taskItem" (keyup.enter)="addTodoTask()" placeholder="Enter..."/>
+  <button (click)="addTodoTask()">Add </button>
+</div>
+<ul>
+  <li *ngFor="let list of todoList; let i = index" draggable="true" (dragstart)="onDragStart(i)" (dragover)="$event.preventDefault()" (drop)="onDrop(i)" [class.draggable]="draggingIndex === i">
+    <ng-container *ngIf="updateIndex!==i; else editMode"> 
+      {{list}}
+      <button (click)="startEdit(i)">Edit</button>
+      <button (click)="deleteTodoTask(i)">Delete</button>
+    </ng-container>
+    <!-- //else must in ng-template only; else it gives error-->
+    <ng-template #editMode>
+      <div style="display: flex; gap: 20px;">  
+        <input type="text" [(ngModel)]="updatingTask" placeholder="Edit.." (keyup.enter)="saveEdit(i)"/>
+        <button (click)="saveEdit(i)">save</button>
+        <button (click)="cancelEdit()">Cancel</button>
+      </div>
+    </ng-template>
+  </li>
+</ul>`
+
+grid =Array.from({length:9})   //gives 9 null items in array=  Array(9)
+grids = Array.from({length:9}, ((_, i)=> i+1))  //add 1-9
+
+lights = Array(9).fill(false);
+
+toggle(index:number){
+  this.lights[index]= !this.lights[index]
+}
+
+gridCode = `grid =Array.from({length:9})   //gives 9 null items in array=  Array(9)
+grids = Array.from({length:9}, ((_, i)=> i+1))  //add 1-9
+
+lights = Array(9).fill(false);
+
+toggle(index:number){
+  this.lights[index]= !this.lights[index]
+}`;
+
+gridHtml = `
+.grid-light {
+  display:grid;
+  grid-template-columns: repeat(3,1fr);
+  gap: 10px;
+}
+.cell-light{
+  height: 80px;
+  cursor: pointer;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  background: 'gold';
+}
+.active{
+  background: blue;
+}
+
+<p>{{grid | json}}</p>
+<p>{{grids | json}}</p>
+
+<div class="grid-light">
+  <div *ngFor="let light of grids; let i = index" class="cell-light" [class.active]="lights[i] ? '': 'light'" (click)="toggle(i)">
+    {{i+1}}
+  </div>
+`;
+isModalOpen = false;
+openModal (){
+  this.isModalOpen=true;
+}
+closeModal(){
+  this.isModalOpen= false;
+}
 }
